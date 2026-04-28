262323da68f4ffacaca7a5c580b5abbb969f09dd
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-04-28 13:13:14 +0400

    Tests updated

--- IEEE754/Program.cs
+++ IEEE754/Program.cs
@@ -4,8 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        float x = 12.345f;
+        float x = 13.625f;
         var fb = FloatBits.FromFloat(x);
+        Console.WriteLine(fb.ToFloat());
         Console.WriteLine(fb.ToBitString(true));
 
         var fromString = FloatBits.FromBitString(fb.ToBitString());
@@ -13,10 +14,10 @@ class Program
 
         double y = -15.5;
         var db = DoubleBits.FromDouble(y);
+        Console.WriteLine(db.ToDouble());
         Console.WriteLine(db.ToBitString(true));
 
         var db2 = DoubleBits.FromBitString(db.ToBitString());
         Console.WriteLine(db2.ToDouble());
-        
     }
 }
