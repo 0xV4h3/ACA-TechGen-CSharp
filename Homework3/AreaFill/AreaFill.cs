256d5fa037d7d72a1f676565f640170a5e48675c
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-01 21:33:25 +0400

    RecursiveTest added

--- Homework3/AreaFill/AreaFill.cs
+++ Homework3/AreaFill/AreaFill.cs
@@ -27,4 +27,25 @@ public static class AreaFill
         Fill(grid, x, y + 1, originalValue, newValue);
         Fill(grid, x, y - 1, originalValue, newValue);
     }
+
+    public static void RecursiveTest()
+    {
+        int[][] grid = [
+            [1, 1, 1, 0, 0, 2, 2, 2],
+            [1, 5, 5, 0, 0, 2, 3, 3],
+            [1, 5, 5, 5, 0, 2, 3, 3],
+            [0, 5, 5, 5, 0, 2, 2, 2],
+            [0, 0, 0, 0, 0, 4, 4, 4],
+            [7, 7, 0, 8, 8, 4, 6, 6],
+            [7, 7, 0, 8, 8, 4, 6, 6],
+            [7, 7, 0, 0, 0, 4, 6, 6]
+        ];
+        
+        Fill(grid, 2, 2, 9);
+
+        foreach (int[] row in grid)
+        {
+            Console.WriteLine(string.Join(" ", row));
+        }
+    }
 }
