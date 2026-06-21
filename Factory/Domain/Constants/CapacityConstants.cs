d34f7b1835f6826de1d6dd6c67a1611a37562f5c
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-21 15:39:54 +0400

    Capacity Constant updated

--- Factory/Domain/Constants/CapacityConstants.cs
+++ Factory/Domain/Constants/CapacityConstants.cs
@@ -1,16 +1,13 @@
 ﻿namespace Domain.Constants;
 
-public class CapacityStates : StateConstant
+public class CapacityState(string value) : StateConstant(value, Contexts.States.Capacity) { }
+
+public static class CapacityStates
 {
-    private CapacityStates(string value) : base(value, Contexts.States.Capacity) { }
+    public static CapacityState Create(string value) => new(value);
 
-    public static CapacityStates Create(string value) => new(value);
-
-    public static readonly CapacityStates Empty = new("Empty");
-    public static readonly CapacityStates PartiallyFull = new("PartiallyFull");
-    public static readonly CapacityStates Full = new("Full");
-    public static readonly CapacityStates Overloaded = new("Overloaded");
-
-    public static IEnumerable<CapacityStates> All => [Empty, PartiallyFull, Full, Overloaded];
+    public static readonly CapacityState Empty = new("Empty");
+    public static readonly CapacityState PartiallyFull = new("PartiallyFull");
+    public static readonly CapacityState Full = new("Full");
+    public static readonly CapacityState Overloaded = new("Overloaded");
 }
-
