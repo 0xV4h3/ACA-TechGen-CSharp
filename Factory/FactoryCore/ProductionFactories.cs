118fd43667103e77afb445873ac5263e05a8f2d1
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-23 03:11:18 +0400

    Item creations removed

--- Factory/FactoryCore/ProductionFactories.cs
+++ Factory/FactoryCore/ProductionFactories.cs
@@ -7,17 +7,14 @@ namespace FactoryCore;
 public class FactoryA(IRegistry registry) : IProductionFactory
 {
     public Machine CreateMachine() => new MachineA(registry);
-    public Item CreateItem(int id) => new ItemA(id, registry);
 }
 
 public class FactoryB(IRegistry registry) : IProductionFactory
 {
     public Machine CreateMachine() => new MachineB(registry);
-    public Item CreateItem(int id) => new ItemB(id, registry);
 }
 
 public class FactoryC(IRegistry registry) : IProductionFactory
 {
     public Machine CreateMachine() => new MachineC(registry);
-    public Item CreateItem(int id) => new ItemC(id, registry);
 }
