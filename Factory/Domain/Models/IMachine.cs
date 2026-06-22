341b3a81fab1d57bd075c26bc67eaa13a1db305b
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-23 03:17:50 +0400

    Machine interfaces created

---
+++ Factory/Domain/Models/IMachine.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Constants;
+
+namespace Domain.Models;
+
+public interface IMachine { }
+
+public interface ISingleTypeMachine : IMachine
+{
+    ItemType SupportedItemType { get; }
+    Item Produce(int id);
+}
+
+public interface IMultiTypeMachine : IMachine
+{
+    List<ItemType> SupportedItemTypes { get; }
+    Item Produce(int id, ItemType type);
+}
