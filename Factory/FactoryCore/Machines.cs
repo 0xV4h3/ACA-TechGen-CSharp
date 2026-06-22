0df5638c3f2e10fea00c7ecbf2b69339a2e14d16
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-23 03:09:40 +0400

    Machines Implementations

--- Factory/FactoryCore/Machines.cs
+++ Factory/FactoryCore/Machines.cs
@@ -4,6 +4,27 @@ using Domain.Registries;
 
 namespace FactoryCore;
 
-public class MachineA(IRegistry registry) : Machine(MachineTypes.MachineA, MachineStates.Idle, [ItemTypes.A], registry) { }
-public class MachineB(IRegistry registry) : Machine(MachineTypes.MachineB, MachineStates.Idle, [ItemTypes.B], registry) { }
-public class MachineC(IRegistry registry) : Machine(MachineTypes.MachineC, MachineStates.Idle, [ItemTypes.C], registry) { }
+public class MachineA(IRegistry registry) : Machine(MachineTypes.MachineA, MachineStates.Idle, [ItemTypes.A], registry)
+{
+    protected override Item CreateItem(int id, ItemType type) => new ItemA(id);
+}
+public class MachineB(IRegistry registry) : Machine(MachineTypes.MachineB, MachineStates.Idle, [ItemTypes.B], registry)
+{
+    protected override Item CreateItem(int id, ItemType type) => new ItemA(id);
+}
+public class MachineC(IRegistry registry) : Machine(MachineTypes.MachineC, MachineStates.Idle, [ItemTypes.C], registry)
+{
+    protected override Item CreateItem(int id, ItemType type) => new ItemA(id);
+}
+
+public class MachineAB(IRegistry registry) 
+    : Machine(MachineTypes.Create("MachineAB"), MachineStates.Idle, [ItemTypes.A, ItemTypes.B], registry)
+{
+    protected override Item CreateItem(int id, ItemType type)
+    {
+        if (type == ItemTypes.A) return new ItemA(id);
+        if (type == ItemTypes.B) return new ItemB(id);
+
+        throw new InvalidOperationException($"MachineAB cannot construct item for type '{type}'.");
+    }
+}
