27c620ba62af21cb9eb7f7f02901161e674ae950
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-23 03:18:19 +0400

    Machine divided to SingleTypeMachine and MultiTypeMachine

--- Factory/Domain/Models/Machine.cs
+++ Factory/Domain/Models/Machine.cs
@@ -4,42 +4,36 @@ using Domain.Exceptions;
 
 namespace Domain.Models;
 
-public abstract class Machine : Entity
+public abstract class Machine : Entity, IMachine
 {
-    public List<ItemType> SupportedItemTypes { get; } = [];
-    
-    protected Machine(MachineType type, MachineState state, List<ItemType> supportedItems, IRegistry registry) 
+    protected Machine(MachineType type, MachineState state, IRegistry registry) 
         : base(type, state)
     {
-        if (supportedItems == null || supportedItems.Count == 0)
-            throw new ArgumentException("Machine must support at least one item type.");
+        if (registry == null) throw new ArgumentNullException(nameof(registry));
         
         if (!registry.IsValid(type.Value, Contexts.Types.Machine))
-            throw new TypeException($"Invalid machine type '{type.Value}' for context '{Contexts.Types.Machine}'", type);
-
-        SupportedItemTypes.AddRange(supportedItems);
+            throw new TypeException($"Invalid machine type '{type.Value}' for context '{Contexts.Types.Machine.Name}'", type);
     }
 
-    public bool CanProduce(ItemType itemType) => SupportedItemTypes.Contains(itemType);
-    
-    public Item Produce(int id, ItemType? type = null)
+    protected virtual bool IsMachineReady() => State != MachineStates.Maintenance;
+    protected virtual void EnsureMachineIsReady()
     {
-        var typeToProduce = type ?? SupportedItemTypes.First();
-
-        if (!CanProduce(typeToProduce))
-            throw new InvalidOperationException($"Machine cannot produce '{typeToProduce}'.");
-        
+        if (State == MachineStates.Maintenance)
+            throw new InvalidOperationException("The machine is undergoing maintenance and cannot produce.");
+    }
+    
+    protected Item RunProductionCycle(Func<Item> createItemLogic, Action beforeProduce, Action<Item> afterProduce)
+    {
         EnsureMachineIsReady();
 
         try
         {
             ChangeState(MachineStates.Producing);
-            OnBeforeProduce(id, typeToProduce);
+            beforeProduce();
             
-            Item createdItem = CreateItem(id, typeToProduce);
+            Item createdItem = createItemLogic();
             
-            OnAfterProduce(createdItem);
-            
+            afterProduce(createdItem);
             return createdItem;
         }
         finally
@@ -47,13 +41,60 @@ public abstract class Machine : Entity
             ChangeState(MachineStates.Idle);
         }
     }
+}
+
+public abstract class SingleTypeMachine(
+    MachineType type, 
+    MachineState state, 
+    ItemType supportedItem, 
+    IRegistry registry) : Machine(type, state, registry), ISingleTypeMachine
+{
+    public ItemType SupportedItemType { get; } = supportedItem ?? throw new ArgumentNullException(nameof(supportedItem));
     
-    // protected virtual bool EnsureMachineIsReady() => State != MachineStates.Maintenance;
-    protected virtual void EnsureMachineIsReady()
+    public Item Produce(int id)
     {
-        if (State == MachineStates.Maintenance)
-            throw new InvalidOperationException("The machine is undergoing maintenance and cannot produce.");
+        return RunProductionCycle(
+            createItemLogic: () => CreateItem(id),
+            beforeProduce: () => OnBeforeProduce(id),
+            afterProduce: OnAfterProduce
+        );
     }
+
+    protected virtual void OnBeforeProduce(int id) { }
+    protected virtual void OnAfterProduce(Item item) { }
+    protected abstract Item CreateItem(int id);
+}
+
+public abstract class MultiTypeMachine : Machine, IMultiTypeMachine
+{
+    public List<ItemType> SupportedItemTypes { get; } = [];
+
+    protected MultiTypeMachine(MachineType type, MachineState state, List<ItemType> supportedItems, IRegistry registry) 
+        : base(type, state, registry)
+    {
+        if (supportedItems == null || supportedItems.Count == 0)
+            throw new ArgumentException("MultiTypeMachine must support at least one item type.", nameof(supportedItems));
+        
+        if (supportedItems.Count < 2)
+            throw new ArgumentException("MultiTypeMachine must support at least two item types. Use SingleTypeMachine instead.");
+
+        SupportedItemTypes.AddRange(supportedItems);
+    }
+    
+    public Item Produce(int id, ItemType type)
+    {
+        if (type == null) throw new ArgumentNullException(nameof(type));
+
+        if (!SupportedItemTypes.Contains(type))
+            throw new InvalidOperationException($"Machine cannot produce '{type}'.");
+
+        return RunProductionCycle(
+            createItemLogic: () => CreateItem(id, type),
+            beforeProduce: () => OnBeforeProduce(id, type),
+            afterProduce: OnAfterProduce
+        );
+    }
+
     protected virtual void OnBeforeProduce(int id, ItemType type) { }
     protected virtual void OnAfterProduce(Item item) { }
     protected abstract Item CreateItem(int id, ItemType type);
