763bb67cca55d45623c4e7e957788a303d8c0e54
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-23 03:19:00 +0400

    Machine Implementations corrected

--- Factory/FactoryCore/Machines.cs
+++ Factory/FactoryCore/Machines.cs
@@ -4,21 +4,21 @@ using Domain.Registries;
 
 namespace FactoryCore;
 
-public class MachineA(IRegistry registry) : Machine(MachineTypes.MachineA, MachineStates.Idle, [ItemTypes.A], registry)
+public class MachineA(IRegistry registry) : SingleTypeMachine(MachineTypes.MachineA, MachineStates.Idle, ItemTypes.A, registry)
 {
-    protected override Item CreateItem(int id, ItemType type) => new ItemA(id);
+    protected override Item CreateItem(int id) => new ItemA(id);
 }
-public class MachineB(IRegistry registry) : Machine(MachineTypes.MachineB, MachineStates.Idle, [ItemTypes.B], registry)
+public class MachineB(IRegistry registry) : SingleTypeMachine(MachineTypes.MachineB, MachineStates.Idle, ItemTypes.B, registry)
 {
-    protected override Item CreateItem(int id, ItemType type) => new ItemA(id);
+    protected override Item CreateItem(int id) => new ItemB(id);
 }
-public class MachineC(IRegistry registry) : Machine(MachineTypes.MachineC, MachineStates.Idle, [ItemTypes.C], registry)
+public class MachineC(IRegistry registry) : SingleTypeMachine(MachineTypes.MachineC, MachineStates.Idle, ItemTypes.C, registry)
 {
-    protected override Item CreateItem(int id, ItemType type) => new ItemA(id);
+    protected override Item CreateItem(int id) => new ItemC(id);
 }
 
 public class MachineAB(IRegistry registry) 
-    : Machine(MachineTypes.Create("MachineAB"), MachineStates.Idle, [ItemTypes.A, ItemTypes.B], registry)
+    : MultiTypeMachine(MachineTypes.Create("MachineAB"), MachineStates.Idle, [ItemTypes.A, ItemTypes.B], registry)
 {
     protected override Item CreateItem(int id, ItemType type)
     {
