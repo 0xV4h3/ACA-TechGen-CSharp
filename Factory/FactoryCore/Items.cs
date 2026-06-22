cd351db3c9701fbe8fed4252423053b8dcc53bbe
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-23 03:10:40 +0400

    Registry removed from Item Inplementations

--- Factory/FactoryCore/Items.cs
+++ Factory/FactoryCore/Items.cs
@@ -4,6 +4,6 @@ using Domain.Registries;
 
 namespace FactoryCore;
 
-public class ItemA(int id, IRegistry registry) : Item(id, ItemTypes.A, ItemStates.Ordered, registry) { }
-public class ItemB(int id, IRegistry registry) : Item(id, ItemTypes.B, ItemStates.Ordered, registry) { }
-public class ItemC(int id, IRegistry registry) : Item(id, ItemTypes.C, ItemStates.Ordered, registry) { }
+public class ItemA(int id) : Item(id, ItemTypes.A, ItemStates.Ordered) { }
+public class ItemB(int id) : Item(id, ItemTypes.B, ItemStates.Ordered) { }
+public class ItemC(int id) : Item(id, ItemTypes.C, ItemStates.Ordered) { }
