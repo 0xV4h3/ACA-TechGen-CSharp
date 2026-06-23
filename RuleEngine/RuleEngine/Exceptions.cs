b5a793d182ca5b692fd2f9d9d9aff97ddbd7a2aa
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-23 15:33:39 +0400

    RuleEngine done

---
+++ RuleEngine/RuleEngine/Exceptions.cs
@@ -0,0 +1,13 @@
+﻿namespace RuleEngine;
+
+public class RuleViolationException(string ruleName, string message) : Exception(message)
+{
+    public string RuleName { get; } = ruleName;
+}
+
+public class EntityValidationException(
+    IEntity entity, RuleViolationException[] violations) : Exception($"{entity.EntityType} #{entity.Id} has {violations.Length} validation error(s).")
+{
+    public IEntity Entity { get; } = entity;
+    public RuleViolationException[] Violations { get; } = violations;
+}
