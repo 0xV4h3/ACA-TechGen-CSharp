15299ee9ec0cd4dabeb161f5be36039fe1b7bbc2
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-27 13:53:42 +0400

    StorageType added

--- Factory/Domain/Constants/StorageConstants.cs
+++ Factory/Domain/Constants/StorageConstants.cs
@@ -1,5 +1,20 @@
 ﻿namespace Domain.Constants;
 
+public class StorageType : TypeConstant
+{
+    internal StorageType(string value) : base(value, Contexts.Types.Storage) { }
+}
+
+public static class StorageTypes
+{
+    public static StorageType Create(string value) => new(value);
+    
+    public static readonly StorageType StorageA = new("StorageA");
+    public static readonly StorageType StorageB = new("StorageB");
+    public static readonly StorageType StorageC = new("StorageC");
+    public static readonly StorageType Unknown = new("Unknown");
+}
+
 public class StorageState : StateConstant
 {
     internal StorageState(string value) : base(value, Contexts.States.Storage) { }
