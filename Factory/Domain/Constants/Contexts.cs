56fa2cd85b5bb5cbc2ae42aae3f6059038fb6f49
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-27 13:31:15 +0400

    Contexts updated

--- Factory/Domain/Constants/Contexts.cs
+++ Factory/Domain/Constants/Contexts.cs
@@ -6,24 +6,34 @@ public static class Contexts
     {
         public static readonly TypeContext Item = new("ItemType");
         public static readonly TypeContext Machine = new("MachineType");
+        public static readonly TypeContext OrderLine = new("OrderLineType");
+        public static readonly TypeContext QualityChecker = new("QualityCheckerType");
+        public static readonly TypeContext Stock = new("StockType");
+        public static readonly TypeContext Transport = new("TransportType");
+        public static readonly TypeContext Storage = new("StorageType");
         
-        public static IEnumerable<TypeContext> All => [Item, Machine];
+        public static IEnumerable<TypeContext> All => 
+        [
+            Item, Machine, OrderLine, QualityChecker,
+            Stock, Transport, Storage
+        ];
     }
 
     public static class States
     {
         public static readonly StateContext Item = new("ItemState");
         public static readonly StateContext Machine = new("MachineState");
-        public static readonly StateContext Capacity = new("CapacityState");
         public static readonly StateContext OrderLine = new("OrderLineState");
+        public static readonly StateContext QualityChecker = new("QualityCheckerState");
         public static readonly StateContext Stock = new("StockState");
-        public static readonly StateContext Storage = new("StorageState");
         public static readonly StateContext Transport = new("TransportState");
+        public static readonly StateContext Storage = new("StorageState");
+        public static readonly StateContext Capacity = new("CapacityState");
         
         public static IEnumerable<StateContext> All => 
         [
-            Item, Machine, Capacity, OrderLine,
-            Stock, Storage, Transport
+            Item, Machine, OrderLine, QualityChecker,
+            Stock, Transport, Storage, Capacity
         ];
     }
 }
