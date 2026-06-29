42588b2789e7f5d36b6ab89959e87e2c3a84d908
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-29 18:45:08 +0400

    Ex02 - Filter and Project : CollectionOps done

---
+++ ExercisesGenerics/CollectionOps/CollectionOps.cs
@@ -0,0 +1,25 @@
+﻿namespace CollectionOps;
+
+public static class CollectionOps
+{
+    public static List<T> Filter<T>(IEnumerable<T> source, Predicate<T> predicate)
+    {
+        var result = new List<T>();
+        foreach (var item in source)
+        {
+            if(predicate(item))
+                result.Add(item);
+        }
+        
+        return result;
+    }
+
+    public static List<TOut> Project<TIn, TOut>(IEnumerable<TIn> source, Func<TIn, TOut> transform)
+    {
+        var result = new List<TOut>();
+        foreach (var item in source)
+            result.Add(transform(item));
+
+        return result;
+    }
+}
