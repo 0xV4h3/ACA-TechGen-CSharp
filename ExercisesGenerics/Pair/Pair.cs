1b3ae9fa7f6d67099626905cd8ed7f62bce4fb07
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-06-29 16:13:27 +0400

    Generic Pair class done

---
+++ ExercisesGenerics/Pair/Pair.cs
@@ -0,0 +1,11 @@
+﻿namespace Pair;
+
+public class Pair<TFirst, TSecond>(TFirst first, TSecond second)
+{
+    public TFirst First { get; } = first;
+    public TSecond Second { get; } = second;
+
+    public Pair<TSecond, TFirst> SwapSides() => new Pair<TSecond, TFirst>(Second, First);
+
+    public override string ToString() => $"({First}, {Second})";
+}
---
+++ ExercisesGenerics/Pair/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Pair;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var p = new Pair<int, string>(7, "seven");
+        var swapped = p.SwapSides();
+
+        Console.WriteLine(p);
+        Console.WriteLine(swapped);
+    }
+}
