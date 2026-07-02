1247c3f4701c714474bdb353b3f22b102ecbcefe
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-02 17:17:42 +0400

    Quality added to Item

--- Factory/Domain/Models/Item.cs
+++ Factory/Domain/Models/Item.cs
@@ -1,9 +1,35 @@
 ﻿using Domain.Constants;
-using Domain.Registries;
 
 namespace Domain.Models;
 
-public abstract class Item(int id, ItemType type, ItemState state) : Entity(type, state)
+public abstract class Item : Entity, IQualitative
 {
-    public int Id { get; init; } = id;
+    private readonly IQualityConverter<ItemQuality> _qualityConverter;
+    public string QualityConverterName { get; private set; }
+
+    public int Id { get; init; }
+    public int QualityPercentage { get; private set; }
+    public ItemQuality Quality { get; private set; }
+
+    protected Item(
+        int id, 
+        ItemType type, 
+        ItemState state, 
+        IQualityConverter<ItemQuality> qualityConverter,
+        int initialPercentage = 100) 
+        : base(type, state)
+    {
+        Id = id;
+        _qualityConverter = qualityConverter ?? throw new ArgumentNullException(nameof(qualityConverter));
+        QualityConverterName = QualityConverters.ForItems.AllNames
+            .FirstOrDefault(name => QualityConverters.ForItems.GetByName(name) == qualityConverter) ?? "Standard";
+
+        ((IQualitative)this).ChangeQuality(initialPercentage); 
+    }
+    
+    void IQualitative.UpdatePercentageInternal(int percentage)
+    {
+        QualityPercentage = percentage;
+        Quality = _qualityConverter.Convert(percentage);
+    }
 }
