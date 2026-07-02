bf2ceb3cf182e0358132ad36b7891ea1e9b5340c
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-02 17:15:33 +0400

    interface for Qualitative entities added

---
+++ Factory/Domain/Models/IQualitative.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Models;
+
+public interface IQualitative
+{
+    int QualityPercentage { get; }
+
+    void ChangeQuality(int newPercentage)
+    {
+        if (newPercentage is < 0 or > 100)
+        {
+            throw new ArgumentOutOfRangeException(nameof(newPercentage), "Quality must be between 0 and 100%.");
+        }
+        
+        UpdatePercentageInternal(newPercentage);
+    }
+    
+    protected void UpdatePercentageInternal(int percentage);
+}
