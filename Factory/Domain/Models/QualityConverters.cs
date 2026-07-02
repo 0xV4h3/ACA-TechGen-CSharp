5bf9794a7e846e07616a7a5867366a3fc9d0725b
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-02 17:17:28 +0400

    QualityConverters for Iten added

---
+++ Factory/Domain/Models/QualityConverters.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Constants;
+using Domain.Registries;
+
+namespace Domain.Models;
+
+public static class QualityConverters
+{
+    public static ItemQualityConverterRegistry ForItems { get; } = new();
+    
+    public sealed class ItemQualityConverterRegistry : QualityConverterRegistry<ItemQuality>
+    {
+        public IQualityConverter<ItemQuality> Standard { get; }
+        public IQualityConverter<ItemQuality> Strict { get; }
+        public IQualityConverter<ItemQuality> Default => Standard;
+
+        internal ItemQualityConverterRegistry()
+        {
+            Standard = Create("Standard", [
+                new(90, 100, ItemQualities.Excellent),
+                new(75, 89,  ItemQualities.Good),
+                new(50, 74,  ItemQualities.Average),
+                new(25, 49,  ItemQualities.Fair),
+                new(0,  24,  ItemQualities.Poor)
+            ]);
+            
+            Strict = Create("Strict", [
+                new(95, 100, ItemQualities.Excellent),
+                new(85, 94,  ItemQualities.Good),
+                new(0,  84,  ItemQualities.Poor)
+            ]);
+        }
+    }
+}
