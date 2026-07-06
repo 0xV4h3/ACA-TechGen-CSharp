4ad4ab5b952a91d8f48293f1866828368dc1ca7a
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-06 22:42:34 +0400

    Models done

---
+++ GenericSpecifications/GenericSpecifications/Models/Candidate.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericSpecifications.Models;
+
+public class Candidate(string id, int experienceYears, decimal salaryAsk, bool isRemoteFriendly, List<string> skills)
+{
+    public string Id { get; } = id;
+    public int ExperienceYears { get; } = experienceYears;
+    public decimal SalaryAsk { get; } = salaryAsk;
+    public bool IsRemoteFriendly { get; } = isRemoteFriendly;
+    public List<string> Skills { get; } = skills;
+}
---
+++ GenericSpecifications/GenericSpecifications/Models/ShipmentOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericSpecifications.Models;
+
+public class ShipmentOrder(string id, decimal weightKg, bool isFragile, bool isExpress, string zone)
+{
+    public string Id { get; }  = id;
+    public decimal WeightKg { get; } = weightKg;
+    public bool IsFragile { get; } = isFragile;
+    public bool IsExpress { get; } = isExpress;
+    public string Zone { get; } = zone;
+}
