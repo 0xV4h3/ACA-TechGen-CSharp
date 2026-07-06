978583a1dec7c4b4067dd6cbded5593a9ecab7ef
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-06 22:43:00 +0400

    Program.cs expected version

--- GenericSpecifications/GenericSpecifications/Program.cs
+++ GenericSpecifications/GenericSpecifications/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace GenericSpecifications;
+﻿using GenericSpecifications.Demo;
+    
+namespace GenericSpecifications;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SpecificationDemo.Run();
     }
 }
