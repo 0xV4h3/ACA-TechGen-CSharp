cfd6de61533bd0943b2b6d668ff3b6141038d3aa
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-09 16:39:39 +0400

    Capacity become Constant Kind

--- Factory/Domain/Constants/CapacityConstants.cs
+++ Factory/Domain/Constants/CapacityConstants.cs
@@ -1,16 +1,16 @@
-﻿namespace Domain.Constants;
-
-public class CapacityState : StateConstant
-{
-    internal CapacityState(string value) : base(value, Contexts.States.Capacity) { }
-}
-
-public static class CapacityStates
-{
-    public static CapacityState Create(string value) => new(value);
-
-    public static readonly CapacityState Empty = new("Empty");
-    public static readonly CapacityState PartiallyFull = new("PartiallyFull");
-    public static readonly CapacityState Full = new("Full");
-    public static readonly CapacityState Overloaded = new("Overloaded");
-}
+﻿// namespace Domain.Constants;
+//
+// public class CapacityState : StateConstant
+// {
+//     internal CapacityState(string value) : base(value, Contexts.Capacity) { }
+// }
+//
+// public static class CapacityStates
+// {
+//     public static CapacityState Create(string value) => new(value);
+//
+//     public static readonly CapacityState Empty = new("Empty");
+//     public static readonly CapacityState PartiallyFull = new("PartiallyFull");
+//     public static readonly CapacityState Full = new("Full");
+//     public static readonly CapacityState Overloaded = new("Overloaded");
+// }
