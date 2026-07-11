9f3848be64e4a4ffb45a29d7b7ae1bff9aaa1870
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-11 20:44:32 +0400

    WriterWorker body

--- SharedMemory/Writer/WriterWorker.cs
+++ SharedMemory/Writer/WriterWorker.cs
@@ -1,6 +1,22 @@
-﻿namespace Writer;
+﻿using System.Text;
+using Domain.Config;
+
+namespace Writer;
 
 public static class WriterWorker
 {
-    
+    public static void Start(string mode, string filePath)
+    {
+        if (File.Exists(filePath)) File.Delete(filePath);
+
+        try
+        {
+            using var fs = new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+            using var sw = new StreamWriter(fs, Encoding.UTF8);
+        }
+        finally
+        {
+            
+        }
+    }
 }
