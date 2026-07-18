a61912a8d806e7c1aeb2d48854597e0a7d77b478
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-18 23:23:45 +0400

    The sequence of properties has been improved

--- PacketSerializer/Simulation/Models/Player.cs
+++ PacketSerializer/Simulation/Models/Player.cs
@@ -5,9 +5,9 @@ public readonly record struct Point(double X, double Y);
 public class Player
 {
     public int Id { get; init; }
-    public Point Coords { get; init; }
     public string Name { get; init; } = string.Empty;
     public int Lvl { get; init; }
+    public Point Coords { get; init; }
     
     public override string ToString() => $"{Id} : {Name} : {Lvl} lvl : (X : {Coords.X}, Y : {Coords.Y})";
 }
