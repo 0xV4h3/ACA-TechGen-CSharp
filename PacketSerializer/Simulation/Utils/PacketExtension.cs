5a29b88b706a4e91a515bb16074c04deda5866b8
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-18 23:24:32 +0400

    Extensions for transformations added

---
+++ PacketSerializer/Simulation/Utils/PacketExtension.cs
@@ -0,0 +1,32 @@
+﻿using Serializer;
+using Simulation.Models;
+
+namespace Simulation.Utils;
+
+public static class PacketExtension
+{
+    public static Player? ToPlayer(this Packet packet)
+    {
+        if (packet.PayloadLength != packet.Payload.Length) 
+            return null;
+
+        return packet.Payload.ToPlayer();
+    }
+}
+
+public static class PacketsExtension
+{
+    public static IReadOnlyList<Player> ToPlayers(this IEnumerable<Packet> packets)
+    {
+        List<Player> players = [];
+        
+        foreach (var packet in packets)
+        {
+            var player = packet.ToPlayer();
+            if (player is not null) 
+                players.Add(player);
+        }
+        
+        return players;
+    }
+}
