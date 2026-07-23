ba598912deb64cd5b83e523c8f0afb1c1680d14f
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-23 20:56:42 +0400

    QualityChecker added Standard Type and  Inspecting replaced with Checking

--- Factory/Domain/Constants/QualityCheckerConstants.cs
+++ Factory/Domain/Constants/QualityCheckerConstants.cs
@@ -9,6 +9,7 @@ public static class QualityCheckerTypes
 {
     public static QualityCheckerType Create(string value) => new(value);
     
+    public static readonly QualityCheckerType Standard = new("Standard");
     public static readonly QualityCheckerType QualityCheckerA = new("A");
     public static readonly QualityCheckerType QualityCheckerB = new("B");
     public static readonly QualityCheckerType QualityCheckerC = new("C");
@@ -25,7 +26,7 @@ public static class QualityCheckerStates
     public static QualityCheckerState Create(string value) => new(value);
     
     public static readonly QualityCheckerState Idle = new("Idle");
-    public static readonly QualityCheckerState Inspecting = new("Inspecting");
+    public static readonly QualityCheckerState Checking = new("Checking");
     public static readonly QualityCheckerState Passed = new("Passed");
     public static readonly QualityCheckerState Failed = new("Failed");
     public static readonly QualityCheckerState Maintenance = new("Maintenance");
