e8cc4c03f60922d4a4986b52bf3a7bd775ac1e11
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-23 21:15:42 +0400

    ForOrderLine and ForStorage Capacity Converters added

--- Factory/Domain/Models/Converters/CapacityConverters.cs
+++ Factory/Domain/Models/Converters/CapacityConverters.cs
@@ -5,8 +5,44 @@ namespace Domain.Models.Converters;
 
 public static class CapacityConverters
 {
+    public static OrderLineCapacityRegistry ForOrderLine { get; } = new();
+    public static StorageCapacityRegistry ForStorage { get; } = new();
     public static StockCapacityRegistry ForStock { get; } = new();
 
+    public sealed class OrderLineCapacityRegistry : RangeConverterRegistry<OrderLineCapacity>
+    {
+        public IRangeConverter<OrderLineCapacity> Standard { get; }
+        public IRangeConverter<OrderLineCapacity> Default => Standard;
+
+        internal OrderLineCapacityRegistry()
+        {
+            Standard = Create("Standard", [
+                new(90, OrderLineCapacities.Full),
+                new(60, OrderLineCapacities.High),
+                new(30, OrderLineCapacities.Moderate),
+                new(1,  OrderLineCapacities.Low),
+                new(0,  OrderLineCapacities.Empty)
+            ]);
+        }
+    }
+
+    public sealed class StorageCapacityRegistry : RangeConverterRegistry<StorageCapacity>
+    {
+        public IRangeConverter<StorageCapacity> Standard { get; }
+        public IRangeConverter<StorageCapacity> Default => Standard;
+
+        internal StorageCapacityRegistry()
+        {
+            Standard = Create("Standard", [
+                new(90, StorageCapacities.Full),
+                new(60, StorageCapacities.High),
+                new(30, StorageCapacities.Moderate),
+                new(1,  StorageCapacities.Low),
+                new(0,  StorageCapacities.Empty)
+            ]);
+        }
+    }
+    
     public sealed class StockCapacityRegistry : RangeConverterRegistry<StockCapacity>
     {
         public IRangeConverter<StockCapacity> Standard { get; }
@@ -15,12 +51,13 @@ public static class CapacityConverters
         internal StockCapacityRegistry()
         {
             Standard = Create("Standard", [
-                new(90, StockCapacities.Full),
-                new(75, StockCapacities.High),
-                new(60, StockCapacities.AboveAverage),
-                new(40, StockCapacities.Moderate),
-                new(25, StockCapacities.BelowAverage),
-                new(1, StockCapacities.Low),
+                new(95, StockCapacities.Overloaded),
+                new(85, StockCapacities.Full),
+                new(65, StockCapacities.High),
+                new(45, StockCapacities.AboveAverage),
+                new(30, StockCapacities.Moderate),
+                new(15, StockCapacities.BelowAverage),
+                new(1,  StockCapacities.Low),
                 new(0,  StockCapacities.Empty)
             ]);
         }
