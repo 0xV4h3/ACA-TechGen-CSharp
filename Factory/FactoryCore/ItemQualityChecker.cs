20848b8317654cd10fe50afe90812ef24e035c01
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-24 20:48:47 +0400

    Concrete QualityChecker for Items

---
+++ Factory/FactoryCore/ItemQualityChecker.cs
@@ -0,0 +1,31 @@
+using Domain.Constants;
+using Domain.Models;
+using Domain.Models.Quality;
+using Domain.Models.Converters;
+using Domain.Registries;
+
+namespace FactoryCore;
+
+public sealed class ItemQualityChecker(
+    List<RangeStep<QualityRoute>> thresholds,
+    IRegistry registry,
+    Action<Item> onPassed,
+    Action<Item> onRepair,
+    Action<Item> onScrap)
+    : QualityChecker(QualityCheckerTypes.Standard, QualityCheckerStates.Idle, thresholds, registry, onPassed, onRepair, onScrap)
+{
+    public static List<RangeStep<QualityRoute>> BuildThresholds(int passPercentage)
+    {
+        if (passPercentage is < 0 or > 100)
+            throw new ArgumentOutOfRangeException(nameof(passPercentage));
+
+        int repairFloor = passPercentage / 2;
+
+        return
+        [
+            new RangeStep<QualityRoute>(passPercentage, QualityRoute.Passed),
+            new RangeStep<QualityRoute>(repairFloor, QualityRoute.Repair),
+            new RangeStep<QualityRoute>(0, QualityRoute.Scrap)
+        ];
+    }
+}
