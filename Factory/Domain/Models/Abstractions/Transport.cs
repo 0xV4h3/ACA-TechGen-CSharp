ed33b9ad0978fe8f6a183eb9fab0bb8505b72b8d
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-24 20:45:22 +0400

    abstraction for Vehicles and Dispatch Strategies

---
+++ Factory/Domain/Models/Abstractions/Transport.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Constants;
+
+namespace Domain.Models.Abstractions;
+
+public interface ITransportVehicle
+{
+    int Capacity { get; }
+    int TicksPerTrip { get; }
+    bool CanCarry(ItemType itemType);
+    
+    void MarkDispatched();
+    void MarkIdle();
+}
+
+public interface ITransportDispatchStrategy
+{
+    ITransportVehicle SelectVehicle(StockOrder order, IReadOnlyList<ITransportVehicle> availableFleet);
+}
