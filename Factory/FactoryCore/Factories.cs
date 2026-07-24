bb1536b4b5f284b1d04703cecb1a03601edda3bf
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-24 20:41:18 +0400

    Factory Method for Machines

---
+++ Factory/FactoryCore/Factories.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+using Domain.Factories;
+using Domain.Models.Quality;
+using Domain.Registries;
+
+namespace FactoryCore;
+
+public class FactoryA(IRegistry registry, IQualityGenerator qualityGenerator) : SingleTypeProductionFactory<MachineA>(registry, qualityGenerator)
+{
+    public override Machine CreateMachine() => new MachineA(QualityGenerator);
+}
+
+public class FactoryB(IRegistry registry, IQualityGenerator qualityGenerator) : SingleTypeProductionFactory<MachineB>(registry, qualityGenerator)
+{
+    public override Machine CreateMachine() => new MachineB(QualityGenerator);
+}
+
+public class FactoryC(IRegistry registry, IQualityGenerator qualityGenerator) : SingleTypeProductionFactory<MachineC>(registry, qualityGenerator)
+{
+    public override Machine CreateMachine() => new MachineC(QualityGenerator);
+}
