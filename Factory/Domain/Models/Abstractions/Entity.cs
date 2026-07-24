5c1ed22a4ddace3f86f245dc0af1b5909dda8882
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-24 20:40:18 +0400

    IEntity removed, Entity now generic

--- Factory/Domain/Models/Abstractions/Entity.cs
+++ Factory/Domain/Models/Abstractions/Entity.cs
@@ -3,22 +3,24 @@ using Domain.Exceptions;
 
 namespace Domain.Models.Abstractions;
 
-public abstract class Entity(TypeConstant type, StateConstant state) : IEntity
+public abstract class Entity<TType, TState>(TType type, TState state)
+    where TType : TypeConstant
+    where TState : StateConstant
 {
-    public TypeConstant Type { get; init; } = type ?? throw new ArgumentNullException(nameof(type));
-    public StateConstant State { get; protected set; } = state ?? throw new ArgumentNullException(nameof(state));
-
-    protected void ChangeState(StateConstant newState)
+    public TType Type { get; init; } = type ?? throw new ArgumentNullException(nameof(type));
+    public TState State { get; protected set; } = state ?? throw new ArgumentNullException(nameof(state));
+    
+    protected void ChangeState(TState newState)
     {
-        if (newState == null) throw new ArgumentNullException(nameof(newState));
-        
+        if (newState is null) throw new ArgumentNullException(nameof(newState));
+
         if (!string.Equals(State.Context.Name, newState.Context.Name, StringComparison.OrdinalIgnoreCase))
         {
             throw new StateException(
                 $"Cannot change state to '{newState.Value}' because its context '{newState.Context.Name}' " +
                 $"does not match the entity state context '{State.Context.Name}'.", newState);
         }
-        
+
         if (string.Equals(State.Value, newState.Value, StringComparison.OrdinalIgnoreCase))
             return;
 
