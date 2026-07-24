286b01c8ade59cd1c5b7101caa022d9c4db52c40
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-24 20:55:25 +0400

    Simulation Running

--- Factory/Simulation/Program.cs
+++ Factory/Simulation/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.Configuration;
+using Domain.Registries;
+using FactoryCore;
 
 namespace Simulation;
 
@@ -6,19 +8,23 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        PrintWelcome();
+
         Configuration.Initialize();
+        IRegistry registry = new Registry();
+
+        var simulation = new FactoryPipelineSimulation(registry);
+        simulation.Run();
+
+        static void PrintWelcome()
+        {
+            Console.WriteLine("==============================================================");
+            Console.WriteLine("          FACTORY PROCESSING & LOGISTICS SIMULATION           ");
+            Console.WriteLine("==============================================================");
+            Console.WriteLine("Flow: Machines -> Order Line -> Quality Checker -> Storage -> Transport -> Stock");
+            Console.WriteLine();
+        }
     }
 }
-
-// using Domain.Configuration;
-// using Domain.Constants;
-// using Domain.Registries;
-//
-// var builder = WebApplication.CreateBuilder(args);
-//
-// var registry = new Registry();
-//
-// foreach (var state in ItemStates.All) registry.Register(state.Value, state.Context);
-// foreach (var type in ItemTypes.All) registry.Register(type.Value, type.Context);
-//
-// builder.Services.AddSingleton<IRegistry>(registry);
