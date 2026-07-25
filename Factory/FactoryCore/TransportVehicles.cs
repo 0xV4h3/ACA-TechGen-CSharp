e73f3b0806a4ea6e7aa16e98dfb716c1b898c151
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-25 12:22:42 +0400

    Concrete Vehicles moved to FactoryCore

---
+++ Factory/FactoryCore/TransportVehicles.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Constants;
+using Domain.Models;
+namespace FactoryCore;
+
+public sealed class Truck() : TransportVehicle(TransportTypes.Truck, capacity: 6, ticksPerTrip: 2)
+{
+    public override bool CanCarry(ItemType itemType) => true;
+}
+
+public sealed class Ship() : TransportVehicle(TransportTypes.Ship, capacity: 40, ticksPerTrip: 10)
+{
+    public override bool CanCarry(ItemType itemType) => true;
+}
+
+public sealed class CargoPlane() : TransportVehicle(TransportTypes.Plane, capacity: 20, ticksPerTrip: 4)
+{
+    public override bool CanCarry(ItemType itemType) => true;
+}
+
+public sealed class Motorcycle() : TransportVehicle(TransportTypes.Motorcycle, capacity: 2, ticksPerTrip: 1)
+{
+    public override bool CanCarry(ItemType itemType) => true;
+}
