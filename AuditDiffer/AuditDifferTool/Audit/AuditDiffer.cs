e8e81dda9a6008084d30887b55b11dcc413306d2
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-30 16:21:55 +0400

    audit tool bluueprint

---
+++ AuditDiffer/AuditDifferTool/Audit/AuditDiffer.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Collections.Concurrent;
+
+namespace AuditDifferTool.Audit;
+
+public static class AuditDiffer
+{
+    private static readonly ConcurrentDictionary<Type, PropertyMetadata[]> PropertyCache = new();
+
+    public static IReadOnlyList<AuditChange> Diff(object? before, object? after, string rootPath = "Order")
+    {
+        var changes = new List<AuditChange>();
+        
+        return changes;
+    }
+}
+
+record PropertyMetadata(PropertyInfo Property, string AuditPathName, Func<object, object?> Getter);
