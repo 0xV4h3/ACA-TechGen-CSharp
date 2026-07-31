65129298d1adc3b998322f957f52df914b589e12
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-31 15:45:45 +0400

    PriceEngineWrapper done

---
+++ PriceEngineWrapper/PriceApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using ACA.PriceEngine;
+using AcaWrapperPriceEngine;
+
+namespace PriceApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PriceEngine engine = new();
+        PriceEngineWrapper engineWrapper = new();
+        PriceInput priceInput = new PriceInput
+        {
+            Lines = [
+                new BasketLine() { Sku = "COFFEE", Quantity = 8, UnitPrice = 10},
+                new BasketLine() { Sku = "TEA", Quantity = 4, UnitPrice = 10}
+            ],
+            LoyaltyTier = 2,
+            CouponAmount = 20m,
+            VatRate = 0.2m
+        };
+
+        decimal wrongAmount = engine.CalculatePayable(priceInput);
+        Console.WriteLine($"Wrong amount: {wrongAmount}");
+        
+        decimal amount = engineWrapper.CalculatePayable(priceInput);
+        Console.WriteLine($"Correct amount: {amount}");
+    }
+}
