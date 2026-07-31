262d662fe81123809772ef0c21d1b091fbe67ad3
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-07-31 22:23:33 +0400

    CsvAttributes added

---
+++ CsvToolkit/CsvSerializerApp/Csv/CsvAttributes.cs
@@ -0,0 +1,11 @@
+﻿namespace CsvSerializerApp.Csv;
+
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class CsvIgnoreAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class CsvColumnAttribute(string header) : Attribute
+{
+    public string Header { get; } = header;
+    public int Order { get; set; } = int.MaxValue;
+}
