a8963fa056c9888dbf06e546aed4fbb40e4a6c21
0xV4h3 <vahe.babayan.dev@gmail.com>
2026-08-07 23:11:59 +0400

    Test Models updated

---
+++ UnitTest/Simulation/Models/MathModel.cs
@@ -0,0 +1,18 @@
+﻿using Engine;
+
+namespace Simulation.Models;
+
+public class MathModel
+{
+    [Test(2, 1)]
+    public int Sum(int a) => a + 1;
+
+    [Test(1, 2)]
+    public int Sub(int a) => a - 1;
+
+    [Test(6, 2, 3)]
+    public int Mul(int a, int b) => a * b;
+
+    [Test(2.5, 5.0, 2.0)]
+    public double Div(double a, double b) => a / b;
+}
